b7628cd3db790eb7bc4704dc0e5898a5d3faaf4c
Altneto <neto1171995@gmail.com>
2022-12-24 12:38:54 -0300

    Testes unitarios

--- src/test/java/br/com/banco/BancoApplicationTests.java
+++ src/test/java/br/com/banco/BancoApplicationTests.java
@@ -1,13 +1,63 @@
 package br.com.banco;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.banco.services.ContaService;
+import br.com.banco.services.TransferenciaService;
+import br.com.banco.vo.ContaNovaVO;
+import br.com.banco.vo.ContaVO;
+import br.com.banco.vo.TransferenciaNovaVO;
+import br.com.banco.vo.TransferenciaVO;
 
 @SpringBootTest
 class BancoApplicationTests {
+	
+	@Autowired
+	private ContaService contaService;
+	
+	@Autowired
+	private TransferenciaService transferenciaService;
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	public ContaVO testarCadastroContaComSucesso() {
+		
+		ContaNovaVO vo = new ContaNovaVO();
+		vo.setNomeResponsavel("Responsavel");
 
+		ContaVO contaVO = new ContaVO();
+		contaVO = contaService.save(vo);
+
+		assertThat(contaVO).isNotNull();
+		assertThat(contaVO.getIdConta()).isNotNull();
+		assertThat(contaVO.getNomeResponsavel()).isNotNull();
+		
+		return contaVO;
+	}
+	
+	@Test
+	public void testarCadastroTransferenciaComSucesso() {
+		
+		ContaVO conta = testarCadastroContaComSucesso();
+		
+		TransferenciaNovaVO vo = new TransferenciaNovaVO();
+		vo.setValor(500.00f);
+		vo.setTipo("Deposito");
+		vo.setNomeOperadorTransacao("Responsavel");
+		vo.setIdConta(conta.getIdConta());
+		vo.setNomeResponsavel(conta.getNomeResponsavel());
+		
+		TransferenciaVO transferenciaVO = new TransferenciaVO();
+		transferenciaVO = transferenciaService.save(vo);
+
+		assertThat(transferenciaVO).isNotNull();
+		assertThat(transferenciaVO.getId()).isNotNull();
+		assertThat(transferenciaVO.getDataTransferencia()).isNotNull();
+		assertThat(transferenciaVO.getValor()).isNotNull();
+		assertThat(transferenciaVO.getTipo()).isNotNull();
+		assertThat(transferenciaVO.getNomeOperadorTransacao()).isNotNull();
+	}
 }
